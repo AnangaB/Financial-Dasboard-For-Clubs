a17375a617bcddd58c74e19752bc331798fdce41
AnangaB <Anangabajgai@gmail.com>
2025-01-18 02:07:19 -0800

    style: made navbar more responsive and added on- hover effects

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,19 +3,33 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <nav className="m-1">
-      <div className="flex p-7 justify-around">
-        <Link href="/">
-          <span>Finances Visualizer</span>
-        </Link>
-        <div className="flex justify-around w-1/2">
-          <Link href="/" aria-current="page">
-            Home
+    <nav className="bg-white">
+      <div className="flex justify-between flex-wrap items-center p-8">
+        <div className="w-full text-2xl sm:w-1/2 lg:w-1/4">
+          <Link className=" hover:bg-sky-400 p-6" href="/">
+            <span>Finances Visualizer</span>
           </Link>
+        </div>
 
-          <Link href="/table" aria-current="page">
-            Data Table
-          </Link>
+        <div className="flex flex-wrap justify-end w-full sm:w-1/2 lg:w-2/3 xl:w-1/3">
+          <div className="w-full text-lg sm:w-1/2 md:w-1/2 lg:w-1/3">
+            <Link
+              className=" hover:bg-sky-400 h-100 p-6"
+              href="/"
+              aria-current="page"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="w-full text-lg sm:w-1/2 md:w-1/2 lg:w-1/3">
+            <Link
+              className=" hover:bg-sky-400 p-6"
+              href="/table"
+              aria-current="page"
+            >
+              Data Table
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
