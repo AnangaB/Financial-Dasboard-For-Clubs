5c28fd00e63f6d80e5bc5cb534c7ae9807ce9b24
AnangaB <Anangabajgai@gmail.com>
2025-01-18 14:53:48 -0800

    fix: added a key to all rows in the data table, to handle unique key not provided error.

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -6,18 +6,28 @@ import { Columns, Table } from "antd";
 import * as d3 from "d3";
 import { useEffect, useState } from "react";
 
-export default function AboutPage() {
+export default function DataTable() {
   const [data, setData] = useState<d3.DSVRowArray<string> | null>(null);
   const [columns, setColumns] = useState<TableColumns[]>([]);
   useEffect(() => {
     setDataAndColumns(setData, setColumns);
   }, []);
 
+  // add a key field to data, so Table component doesn't output err about unique key
+  const processedData = data
+    ? data.map((row, index) => ({
+        ...row,
+        key: index,
+      }))
+    : [];
+
   return (
     <div className="min-h-screen">
       <Navbar />
       <h1 className="text-center text-2xl">Data Table</h1>
-      {data != null && <Table dataSource={data} columns={columns as Columns} />}
+      {processedData.length > 0 && (
+        <Table dataSource={processedData} columns={columns as Columns} />
+      )}
     </div>
   );
 }
