89dd68a3035861d2416e14f42dc58af2d6af1676
AnangaB <Anangabajgai@gmail.com>
2025-01-29 22:54:41 -0800

    style: changed Semester Bar's semester texts into buttons

--- src/components/common/SemesterBar.tsx
+++ src/components/common/SemesterBar.tsx
@@ -18,8 +18,8 @@ export default function SemesterBar({
             <button
               key={s}
               className={`${
-                activeSemester == s ? "text-red-700" : "hover:text-red-800"
-              } p-1`}
+                activeSemester == s ? " bg-red-700" : "bg-blue-700"
+              } p-1 hover:bg-red-800 text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
               onClick={() => setActiveSemester(s)}
             >
               {s}
